baa0b16b2be8f769c6fedec13b7d202fbc1d1d3d
JosePascua01 <joelo.pascua@gmail.com>
2024-11-27 13:19:02 -0500

    renamed:    home.html -> index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 let intro = document.querySelector('.intro');
 let sections = document.querySelectorAll('section'); // stores all sections in an array  sections = [home-page, capture-page...]
-let backBtn = document.querySelector('button.back-btn')
+let backBtn = document.querySelector('button#back-btn')
 /*This Section is the loading page of the application*/
 window.addEventListener('DOMContentLoaded', () => {
     setTimeout(
         () => {
             intro.style.left = '-100vw';
-            appStart.startHome();
+            appLogics.startHome();
         },
         3000
     )
 })//end of window.eventListener
 
 //console.log(sections.length);
-const appStart = (() => {
+const appLogics = (() => {
 
     const startHome = () =>{
         return document.querySelector('#home-page').classList.remove('hidden'), document.querySelector('#home-page').classList.add('active');
     }
 
-    const showSection = (sectionId, currentIndex) => { //showSection = function, sectionId = 
-        const link = document.querySelector(`#${sectionId}`);
+    const showSection = (sectionId, currentIndex) => { //showSection = function, sectionId =
+/*        console.log(backBtn); 
+        console.log(sectionId);*/
+        const link = document.querySelector(`section#${sectionId}`);
         console.log(`currentIndex = ${currentIndex}`);
         link.classList.add('active');
         link.classList.remove('hidden');
 
-        sectionId === 'home-page' ? backBtn.classList.add('hidden') : backBtn.classList.remove('hidden'); //is the current section shown the homepage? then add back button otherwise remove
+        sectionId !== 'home-page' ? backBtn.classList.remove('hidden') : backBtn.classList.add('hidden'); //if the current section shown is not homepage then add back button otherwise remove
 
         sections.forEach((section) => { //goes through all section element inside of the sections array
             if (section.id !== sectionId) { //if the current section id is not the current section being shown, hide that section
